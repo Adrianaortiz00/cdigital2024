28f2f0b2d8cfa6025bbc19cb6ee1e9adc7bcb36f
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2025-02-13 05:54:26 -0600

    adding component slider services and his styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Button from "@/components/Button";
 import SliderMain from "@/components/SliderMain";
 import ContactForm from "@/components/ContactForm";
 import SliderLogos from "@/components/SliderLogos";
+import SliderServices from "@/components/SliderServices";
 
 export const metadata = {
   title: "Inicio | CDigital | Soluciones en Power Platform",
@@ -14,35 +15,7 @@ export default function Page() {
     <div>
       <SliderMain/>
       <SliderLogos/>
-      <Button 
-        children={"holi peque"}
-        variant = {"green-button"}
-        size = {"small"} 
-      />
-      <Button 
-        children={"holi verde"}
-        variant = {"green-button"}
-        size = {"medium"} 
-      />
-      <Button 
-        children={"holi azul"}
-        variant = {"blue-button"}
-        size = {"medium"} 
-      />
-      <Button 
-        children={"holi azul grande"}
-        variant = {"blue-button"}
-        size = {"large"} 
-      />
-      <h1 className="title">Space Grotesk | Bold 60 | Title h1</h1>
-      <h2 className="subtitle">
-        Space Grotesk | Medium 48 | Subtitle h2
-      </h2>
-      <h3 className="description">
-        Poppins | Regular 30 | Descriptions h3
-      </h3>
-      <p className="text-p">Poppins | Regular 18 | Text p</p>
-      <h4 className="text-h">Poppins | SemiBold 18 | Text h</h4>
+      <SliderServices/>
       <ContactForm/>
     </div>
   );
---
+++ src/components/SliderServices.jsx
@@ -0,0 +1,83 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+import SubtitleSection from "./SubtitleSection"
+import styles from "@/styles/components/sliderServices.module.scss";
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+const slidesService=[
+    {
+        id:1,
+        title:"Title 1",
+        image: "/images/slide-service-1.png",
+    },
+    {
+        id:2,
+        title:"Title 2",
+        image: "/images/slide-service-1.png",
+    },
+    {
+        id:3,
+        title:"Title 3",
+        image: "/images/slide-service-1.png",
+    },
+    {
+        id:4,
+        title:"Title 4",
+        image: "/images/slide-service-1.png",
+    },
+    {
+        id:5,
+        title:"Title 5",
+        image: "/images/slide-service-1.png",
+    },
+]
+function SliderServices() {
+  return (
+    <section className={styles.section}>
+        <div className={styles.services}>
+        <SubtitleSection
+            title="Soluciones Tecnológicas"
+            description="This is a description of the section in center alignment. It could have 2 lines per subtitle."
+            alignment="center"
+            color="white"
+        />
+        <Swiper
+            effect={'coverflow'}
+            coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 2.5,
+              }}
+            modules={[Navigation, Pagination, Autoplay,]}
+            spaceBetween={10}
+            slidesPerView={6}
+            loop={true} 
+            className={styles["slider"]}
+        >
+            {slidesService.map((slide)=>(
+                <SwiperSlide key={slide.id} className={styles["slider-single"]}>
+                {({isActive})=>(
+                    <div>
+                    <Image 
+                    src={slide.image}
+                    alt="imagen de parner"
+                    width={100}
+                    height={100}
+                    layout="intrinsic"
+                    className={styles["image"]}
+                    />
+                    <h3>{slide.title}</h3>
+                    </div>
+                )}
+                </SwiperSlide>
+            ))}
+            
+        </Swiper>
+        </div>
+    </section>
+  )
+}
+
+export default SliderServices
