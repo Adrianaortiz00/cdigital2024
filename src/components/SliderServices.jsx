054ea8dc38baf8bc74657bf5273a5b85468fc09c
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2025-02-20 10:40:51 -0600

    adding images and styles

--- src/components/SliderServices.jsx
+++ src/components/SliderServices.jsx
@@ -16,7 +16,7 @@ const SwiperSlideComponent = dynamic(() =>
  */
 
 import Image from "next/image";
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { EffectCoverflow, Navigation } from 'swiper/modules';
 import SubtitleSection from "./SubtitleSection"
 import styles from "@/styles/components/sliderServices.module.scss";
 import 'swiper/css/effect-coverflow';
@@ -29,27 +29,27 @@ const slidesService=[
     {
         id:1,
         title:"Power Apps",
-        image: "/images/slide-service-1.png",
+        image: "/images/slide-service-power-apps.svg",
     },
     {
         id:2,
         title:"Power Automate",
-        image: "/images/slide-service-1.png",
+        image: "/images/slide-service-power-automate.svg",
     },
     {
         id:3,
         title:"SharePoint",
-        image: "/images/slide-service-1.png",
+        image: "/images/slide-service-sharepoint.svg",
     },
     {
         id:4,
         title:"UX/UI",
-        image: "/images/slide-service-1.png",
+        image: "/images/slide-service-ux-ui.svg",
     },
     {
         id:5,
         title:"Capacitaciones & Soporte",
-        image: "/images/slide-service-1.png",
+        image: "/images/slide-service-capacitaciones-y-soporte.svg",
     },
 ]
 function SliderServices() {
@@ -66,20 +66,20 @@ function SliderServices() {
             effect="coverflow"
             coverflowEffect={{
                 rotate: 0,
-                stretch: 0,
-                depth: 100,
-                modifier: 2.5,
+                stretch: 10,
+                depth: 190,
+                modifier: 1,
                 slideShadows: false
               }}
-            modules={[EffectCoverflow, Navigation, Pagination, Autoplay,]}
+            modules={[EffectCoverflow, Navigation, Autoplay,]}
             centeredSlides={true}
-            spaceBetween={0}
-            slidesPerView={3}
-           /*  loop={true} 
+            //spaceBetween={0}
+            slidesPerView={'auto'}
+            loop={true} 
             autoplay={{
                 delay: 2500,
                 disableOnInteraction: false
-            }} */
+            }} 
             //pagination={{ clickable: true }}
             navigation={true}
             className={styles["slider"]}
@@ -91,8 +91,8 @@ function SliderServices() {
                     <Image
                     src={slide.image}
                     alt="imagen de parner"
-                    width={100}
-                    height={100}
+                    width={150}
+                    height={150}
                     layout="intrinsic"
                     className={styles["image"]}
                     />
